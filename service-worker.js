6d0e45ac4572756ff1390fb89eb538d9e68b9ebb
sir-vik <samuelchosen57@gmail.com>
2026-09-12 13:27:13 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,40 @@
-const CACHE_NAME = "neighbourly-v3";
+const CACHE_NAME = "neighbourly-v4";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./neighbourly-icon-192.png",
+  "./neighbourly-icon-512.png"
+];
 
 self.addEventListener("install", event => {
-  console.log("Neighbourly service worker installing...");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_FILES);
+    })
+  );
+
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys().then(keys => {
+      return Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
